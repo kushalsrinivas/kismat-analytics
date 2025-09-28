b8e5cf68d9f7573f7e19a22da016d843e8a70590
kushalsrinivas <kushal.s.2005@gmail.com>
2025-09-28 22:09:38 +0530

    removed auth for view

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,13 +1,5 @@
-import { auth } from "~/server/auth";
-import { redirect } from "next/navigation";
 import { AnalyticsDashboard } from "~/app/_components/analytics/analytics-dashboard";
 
 export default async function AnalyticsPage() {
-  const session = await auth();
-
-  if (!session?.user) {
-    redirect("/api/auth/signin");
-  }
-
   return <AnalyticsDashboard />;
 }
